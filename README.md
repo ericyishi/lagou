2a8848485be5a4dca52f4742fab9ecc071f358db
ericyishi <eric_yishi@163.com>
2020-05-22 15:53:13 +0800

    lagou spider in selenium3

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # lagou
-使用selenium 获取职位基本信息
+* 使用selenium 获取职位基本信息，方便自己筛选
+* 只是爬取了职位基本信息：职位、公司、行业、发布时间、基本信息、薪酬、网址
+* 使用是selenium3模拟用户界面方式来获取数据
+### 注意事项
+* selenium3的webdriver需要自己指明其驱动路径，这里使用的chrome的
+* 另外配置了无头浏览器,可以运行时候不显示浏览器
+  ```
+    self.chrome_options = Options()
+    self.chrome_options.add_argument('--headless')
+	self.driver = webdriver.Chrome(executable_path=self.chromepath,chrome_options=self.chrome_options)
+
+  ```
+* 在使用selenuim的xpath获取属性值时，需要使用方法get_attribute()
+  ```
+    a=li.find_element_by_xpath(".//div[@class='list_item_top']//div[@class='p_top']//a")
+    item_list.append(a.get_attribute('href'))
+  ```  
+* CSV中文写入乱码，open文件时候指定编码为：encoding='utf-8-sig'
+  ```
+     with open(self.search_name + "_data.csv", "a", encoding='utf-8-sig') as f:
+  ```  
+### 待优化点 
+  1. 运行时间
+  2. 生成csv增加当前时间
+  3. 选择城市的时候，目前只能传入热门城市（//a[@class="hot-city-name"）
+  
---
+++ lagou.py
@@ -0,0 +1,110 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+
+class Lagou(object):
+    def __init__(self, search_name,city):
+        self.start_url = "https://www.lagou.com/jobs/list_%E6%B5%8B%E8%AF%95?labelWords=&fromSearch=true&suginput="
+        self.city=city
+        self.search_name = search_name
+        self.chrome_options = Options()
+        self.chrome_options.add_argument('--headless')
+        self.chromepath=r'E:\installPackage\selenium_driver\chromedriver.exe'
+
+        # self.driver = webdriver.Chrome(chrome_options=self.chrome_options)
+
+        self.driver = webdriver.Chrome(executable_path=self.chromepath,chrome_options=self.chrome_options)
+        self.content_header = ["positionName", "businessZones", "CreateTime", "companyShortName", "salary", "workYear"]
+
+    def isElementExists(self, by, value):
+        # 先导入异常包
+        from selenium.common.exceptions import NoSuchElementException
+        try:
+             self.driver.find_element(by=by, value=value)
+        except NoSuchElementException as e:
+            print(e)
+            return False
+        else:
+            return True
+
+    def close_ad(self):
+        time.sleep(2)
+        if self.isElementExists("class name","body-btn"):
+            self.driver.find_element_by_css_selector(".body-btn").click()
+        # else:
+        #     print("no ad")
+        # .body-btn
+    def choose_city(self): # 选择城市
+        time.sleep(2)
+        self.driver.find_element_by_xpath('//a[@class="hot-city-name" and text()="'+self.city+'"]').click()
+
+    def get_content_list(self):  # 提取数据
+        li_list = self.driver.find_elements_by_xpath('//li[contains(@class, "con_list_item")]')
+        content_list = []
+        for li in li_list:
+            item_list = []
+            # title
+            item_list.append(li.find_element_by_xpath(".//div[@class='list_item_top']//a//h3").text)
+            # 发布时间
+            item_list.append(li.find_elements_by_xpath(".//div[@class='list_item_top']//span[@class='format-time']")[0].text)
+
+            # 公司名
+            item_list.append(li.find_elements_by_xpath(".//div[@class='list_item_top']//div[@class='company_name']/a")[0].text)
+            # 行业
+            item_list.append(
+                li.find_elements_by_xpath(".//div[@class='list_item_top']//div[@class='industry']")[0].text)
+
+            # 薪酬
+            item_list.append(li.find_elements_by_xpath(".//div[@class='list_item_top']//div[@class='p_bot']//span[@class='money']")[0].text)
+            # 经验
+            item_list.append(li.find_elements_by_xpath(".//div[@class='list_item_top']//div[@class='p_bot']//div[@class='li_b_l']")[0].text)
+
+            # 链接
+            a=li.find_element_by_xpath(".//div[@class='list_item_top']//div[@class='p_top']//a")
+            item_list.append(a.get_attribute('href'))
+
+            # print(item_list)
+            content_list.append(item_list)
+
+        # 下一页
+        next_url = self.driver.find_elements_by_xpath("//span[@class='pager_next ']")
+        next_url = next_url[0] if len(next_url) > 0 else None
+        return content_list, next_url
+
+    def save_content_list(self, content_list):
+        with open(self.search_name + "_data.csv", "a", encoding='utf-8-sig') as f:
+            for content in content_list:
+                print(content)
+                for item in content:
+                    if isinstance(item, str):
+                        if '，' in item:
+                            item.replace(",", "|")
+                    f.write(item+ ",")
+                f.write('\n')
+
+    def run(self):
+        # 发送请求
+        self.driver.get(self.start_url)
+        self.close_ad()
+        self.choose_city()
+
+        #
+        # # 提取数据
+        content_list, next_url = self.get_content_list()
+        #
+        # # 保存数据
+        self.save_content_list(content_list)
+        #
+        # # 翻页
+        while next_url is not None:
+            next_url.click()
+            time.sleep(6)
+            content_list, next_url = self.get_content_list()
+            self.save_content_list(content_list)
+
+
+if __name__ == '__main__':
+    lagou = Lagou("测试","成都")
+    lagou.run()
+
